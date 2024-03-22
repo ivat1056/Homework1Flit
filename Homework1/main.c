66f31ff1a3c07fb77274de4219cf8ecff70ef8b3
ivat1056 <ivat1056@gmail.com>
2024-03-22 19:15:18 +0300

    Initial Commit

---
+++ Homework1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Homework1
+//
+//  Created by Ivan Matveev on 22.03.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
